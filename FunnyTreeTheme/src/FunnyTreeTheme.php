c7d3e7d01d51ac7de2d7dd026cb86a3cb1fd2db3
Oliverhorn <horn.oliver@gmail.com>
2025-09-28 19:53:10 +0200

    initial_commit

---
+++ FunnyTreeTheme/src/FunnyTreeTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace FunnyTreeTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class FunnyTreeTheme extends Plugin implements ThemeInterface
+{
+}
